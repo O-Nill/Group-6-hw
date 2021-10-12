e0da65628d852be7bf8d3942dd01f6880359219b
pavel <rum787@mail.ru>
2021-10-12 21:31:08 +0500

    11.10.21

--- site_27.09.21/index.php
+++ site_27.09.21/index.php
@@ -1,3 +1,35 @@
+<?
+    $about = 'Родился в городе Магнитогорск. Закончил школу, институт. Нравится чинить технику,
+ программировать, путешествовать. Работал на разных специальностях: сборщик ПК, программист и даже продавец.
+ И точно я не писатель)';
+    $comment = 'Мне понравились курсы от Факт. Они позволяют в короткие сроки сдать экзамен по Битрикс,
+ а значит получить достойную работу в компании. Преподаются курсы с большим количеством примеров и хорошим
+ вовлечением учащихся)';
+    $abOut = "<font color = blue>".mb_substr($about, 0,mb_strpos($about, '.')+1)."</font>".
+        mb_substr($about, mb_strpos($about, '.')+1);
+    $i = 0;
+    $comOut = "";
+    $vowels  = ['а', 'о', 'у', 'э', 'ы', 'я', 'ё', 'ю', 'е', 'и', 'А', 'О', 'У', 'Э', 'Ы', 'Я', 'Ё', 'Ю', 'Е', 'И'];
+    foreach (explode(' ', $comment) as $value) {
+        $i++;
+        if ($i % 2 !== 0) {
+            $comOut .= "<font color = blue>".$value.' '."</font>";
+        }
+        else {
+            $comOut .= "<font color = #663399>".$value.' '."</font>";
+        }
+    }
+    $result = 0;
+    $text = $about.' '.$comment;
+    for ($i = 0; $i < count($vowels); $i++) {
+        $result += mb_substr_count($text, $vowels[$i]);
+    }
+    $strBirthday = '1990-07-16';
+    $strTime = date("Y-m-d");
+    $birthday = date_create($strBirthday);
+    $time = date_create($strTime);
+    $interval = date_diff($birthday, $time);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,18 +45,22 @@
         <div class="menu_item"><a href="table.php">Таблица</a></div>
         <div class="menu_item"><a href="task.php">Задача PHP</a></div>
         <div class="menu_item"><a href="arrays.php">Массивы</a></div>
-        <div class="menu_item"><a href="images/my_view.png">Скрин</a></div>
+        <div class="menu_item"><a href="images/tasks4_5.jpg">Задачи 4, 5</a></div>
     </section>
 </header>
 <main>
     <div class="photo"><img src="images/me.png" title="It's not me)" width="100%"  alt="Photo"></div>
     <h1 class="name">Румянцев Павел</h1>
-    <p class="about pad">Родился в городе Магнитогорск. Закончил школу, институт. Нравится чинить технику,
-        программировать, путешествовать. Работал на разных специальностях: сборщик ПК, программист и даже продавец.
-        И точно я не писатель)</p>
-    <p class="liked pad">Мне понравились курсы от Факт. Они позволяют в короткие сроки сдать экзамен по Битрикс,
-        а значит получить достойную работу в компании. Преподаются курсы с большим количеством примеров и хорошим
-        вовлечением учащихся)</p>
+    <p class="about pad">
+        <?
+            echo $abOut;
+        ?>
+    </p>
+    <p class="liked pad">
+        <?
+            echo $comOut;
+        ?>
+    </p>
     <section class="titles">
     <div class="title">Любимые животные из YouTube</div>
     <div class="title">YouTube каналы с данными животными</div>
@@ -94,5 +130,14 @@
         </div>
     </section>
 </main>
+<footer>
+    <?
+        echo 'Количество гласных букв на странице: '.$result."<br>";
+        echo 'Количество слов на странице: '.(mb_substr_count($text, ' ')+1)."<br>";
+        echo 'Дата рождения: '.$strBirthday."<br>";
+        echo 'Текущая дата: '.$strTime."<br>";
+        echo 'Количество дней между датами: '.$interval->format('%a');
+    ?>
+</footer>
 </body>
 </html>
